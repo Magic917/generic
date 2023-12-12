effba68800d54e0f2f8fe87ad8a7a3e820cfd0c0
Magic917 <magic.time917@gmail.com>
2023-12-11 21:33:08 -0500

    update output

--- generic/MyAnswer.cs
+++ generic/MyAnswer.cs
@@ -6,8 +6,36 @@ using System.Threading.Tasks;
 
 namespace generic
 {
-    internal class MyAnswer
+    public class MyAnswer<T> where T : struct
     {
+        private List<T> list;
+        public MyAnswer() 
+        {
+            list = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            list.Add(item);
+        }
+        public T GetItem(int index)
+        {
+
+            if (index >= 0 && index < list.Count)
+            {
+                return list[index];
+            }
+            else
+            {
+                throw new IndexOutOfRangeException();
+            };
+        }
+        public List<T> GetOrderList()
+        {
+            List<T> Orderedlist = new List<T>(list);
+            Orderedlist.Sort();
+            return Orderedlist;
+        }
     }
 }
 
--- generic/Program.cs
+++ generic/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            MyAnswer<int> answer = new MyAnswer<int>();
+            answer.Add(4);
+            answer.Add(5);
+            answer.Add(3);
+            answer.Add(1);
+            answer.Add(2);
+            answer.Add(6);
+            Console.WriteLine(string.Join(",",answer.GetOrderList()));
         }
     }
 
